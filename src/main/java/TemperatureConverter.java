45bc32f338aadf3ae3ab4bf905b46c61ea04ced2
mohammedsadik99 <mohammmedsadikaziz@gmail.com>
2024-02-19 17:23:25 +0200

    added target and mvn tests

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -1,28 +1,10 @@
 public class TemperatureConverter {
 
-    public static float farToCel(float far){
+    public static float farToCel(float far) {
         return (far - 32) * 5 / 9;
     }
 
-    public static float kelvinToCel(float kel){
+    public static float kelvinToCel(float kel) {
         return (float) (kel - 273.15);
-    }
-
-    public static void main(String[] args) {
-        // Test farToCel function
-        float fahrenheit = 100;
-        float expectedCelsius = 37.77778f;
-        float actualCelsius = farToCel(fahrenheit);
-        System.out.println("Fahrenheit to Celsius:");
-        System.out.println("Expected: " + expectedCelsius);
-        System.out.println("Actual: " + actualCelsius);
-
-        // Test kelvinToCel function
-        float kelvin = 273.15f;
-        float expectedCelsiusFromKelvin = 0;
-        float actualCelsiusFromKelvin = kelvinToCel(kelvin);
-        System.out.println("\nKelvin to Celsius:");
-        System.out.println("Expected: " + expectedCelsiusFromKelvin);
-        System.out.println("Actual: " + actualCelsiusFromKelvin);
     }
 }
--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -1,7 +1,15 @@
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class TemperatureConverterTest {
+
+    @Test
+    public void testFarToCel() {
+        float fahrenheit = 100;
+        float expectedCelsius = 37.77778f;
+        float actualCelsius = TemperatureConverter.farToCel(fahrenheit);
+        assertEquals(expectedCelsius, actualCelsius, 0.0001);
+    }
 
     @Test
     public void testKelvinToCel() {
