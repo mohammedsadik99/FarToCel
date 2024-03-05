7992c4f87ef21ed2ea73eb631bac290fda1f9a8a
mohammedsadik99 <mohammmedsadikaziz@gmail.com>
2024-03-05 14:58:04 +0200

    changes

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -4,7 +4,15 @@ public class TemperatureConverter {
         return (far - 32) * 5 / 9;
     }
 
+    public static float celToFar(float cel) {
+        return cel * 9 / 5 + 32;
+    }
+
     public static float kelvinToCel(float kel) {
-        return (float) (kel - 273.15);
+        return kel - 273.15f;
+    }
+
+    public static float kelvinToFar(float kel) {
+        return (kel - 273.15f) * (9.0f / 5.0f) + 32;
     }
 }
---
+++ src/main/java/TemperatureConverterMain.java
@@ -0,0 +1,9 @@
+public class TemperatureConverterMain {
+    public static void main(String[] args) {
+        float kelvinTemp = 300.1f;
+        float celsiusTemp = TemperatureConverter.kelvinToCel(kelvinTemp);
+        float fahrenheitTemp = TemperatureConverter.kelvinToFar(kelvinTemp);
+        System.out.println(kelvinTemp + " Kelvin is equal to " + celsiusTemp + " Celsius.");
+        System.out.println(kelvinTemp + " Kelvin is equal to " + fahrenheitTemp + " Fahrenheit.");
+    }
+}
